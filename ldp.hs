1d5b1f9c78bdc521e980d6b3b1692ae3026db069
Marco Villotta <gmarcovil@gmail.com>
2021-06-16 16:39:16 +0200

    16 giugno

--- ldp.hs
+++ ldp.hs
@@ -98,10 +98,49 @@ map4 f xs = foldl (\xs x -> xs ++ [f x]) [] xs
 -- map con foldr
 map5 f xs = foldr (\x xs -> f x : xs) [] xs
 
-data BTree a = Null | Bt a (Bt a) (Bt a)
+data BTree a = Null | BTree a (BTree a) (BTree a)
 
 sumBT Null = 0
-sumBT (Bt n tL tR) = n + sumBT tL + sumBT tL
+sumBT (BTree n tL tR) = n + sumBT tL + sumBT tL
 
 depthBT Null = 0
-depthBT (Bt n tL tR) = 1 + max (depthBT tL) (depthBT tR)
+depthBT (BTree n tL tR) = 1 + max (depthBT tL) (depthBT tR)
+
+takeDepthBT 0 _ = Null
+takeDepthBT _ Null = Null
+takeDepthBT n (BTree a tL tR) = (BTree a (takeDepthBT (n-1) tL) (takeDepthBT (n-1) tR))
+
+insertBST x Null = BTree x Null Null
+insertBST x (BTree y tL tR) = if x < y then (BTree y (insertBST x tL) tR) else (BTree y tL (insertBST x tR))
+-- eventualmente si poteva scrivere con le guardie invece dell if then else
+
+-- da albero a lista con visita anticipata
+visitBST Null = []
+visitBST (BTree y tL tR) = visitBST tL ++ y : visitBST tR
+-- l'operazione di concatenazione non la rende molto efficiente
+-- per concatenare deve svolgere tutta la prima lista
+
+visitBST2 tree = visitBST2Aux tree [] where
+  visitBST2Aux Null acc = acc
+  visitBST2Aux (BTree x tL tR) acc = visitBST2Aux tL (x : visitBST2Aux tR acc) 
+
+b1 = (insertBST 12 (insertBST 67 (insertBST 23 (insertBST 56 (insertBST 31 (insertBST 23 (insertBST 40 Null)))))))
+
+-- da lista a albero
+listToBST xs = listToBSTAux xs Null where
+  listToBSTAux [] bst = bst
+  listToBSTAux (x:xs) bst = listToBSTAux xs (insertBST x bst)
+
+-- quindi da lista posto costrure albero e poi fare albero
+-- e ottenere un algoritmo di ordinamento
+
+-- ma si potrebbe anche definire con foldl
+listToBST2 xs = foldl (\bst x -> insertBST x bst) Null xs
+
+-- oppure con foldr
+listToBST3 xs = foldr insertBST Null xs
+-- potevo anche scriverle senza xs da argomenti e corpo
+
+-- ottengo alberi diversi con le due ...
+
+ordina xs = visitBST2 (listToBST3 xs)
